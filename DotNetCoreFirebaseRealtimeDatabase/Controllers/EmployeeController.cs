c2aae8b54f2e8d6b26db6554a81ff4691a7c3828
zerdasoftware <zerdasoftware@gmail.com>
2022-01-15 23:04:33 +0300

    Add project files.

---
+++ DotNetCoreFirebaseRealtimeDatabase/Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using DotNetCoreFirebaseRealtimeDatabase.Models;
+
+namespace DotNetCoreFirebaseRealtimeDatabase.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "EhYavvhhnarP8qAD9R7e4mPKC1NZ1jZxgTvmnpsb",
+            BasePath = "https://fir-login-ce5d8-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        public ActionResult Index()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Employee");
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+            var list = new List<Employee>();
+            foreach (var item in data)
+            {
+                list.Add(JsonConvert.DeserializeObject<Employee>(((JProperty)item).Value.ToString()));
+            }
+            return View(list);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                AddEmployeeToFirebase(employee);
+                ModelState.AddModelError(String.Empty, "Added Succesfuly");
+            }
+            catch (Exception ex)
+            {
+                throw;
+                ModelState.AddModelError(String.Empty, ex.Message);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        private void AddEmployeeToFirebase(Employee employee)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            var data = employee;
+            PushResponse response = client.Push("Employee/", data);
+            data.id = response.Result.name;
+            SetResponse setresponse = client.Set("Employee/" + data.id, data);
+        }
+
+        [HttpGet]
+        public ActionResult Detail(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Employee/+id");
+            Employee data = JsonConvert.DeserializeObject<Employee>(response.Body);
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            FirebaseResponse response = client.Get("Employee/+id");
+            Employee data = JsonConvert.DeserializeObject<Employee>(response.Body);
+            return View(data);
+        }
+    }
+}
---
+++ DotNetCoreFirebaseRealtimeDatabase/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetCoreFirebaseRealtimeDatabase.Models
+{
+    public class Employee
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string deadline { get; set; }
+
+        public string AStartTime { get; set; }
+        public string AFinishTime { get; set; }
+        public string BStartTime { get; set; }
+        public string BFinishTime { get; set; }
+    }
+}
